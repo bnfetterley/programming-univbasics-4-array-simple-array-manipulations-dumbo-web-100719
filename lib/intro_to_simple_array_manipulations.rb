73638b1781162b1a1b3fd7e0b5d3ababa82f7e16
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 20:09:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
 def using_push(array, element)
+<<<<<<< HEAD
   array.push(element)
   end
 
@@ -46,3 +47,23 @@ def using_delete_at(array, integer)
   array.delete_at(2)
 end
   
+=======
+   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+   colors_in_the_rainbow.push("violet")
+  end
+
+def using_unshift(array, element)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
+
+def using_pop_with_args(array, integer)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
+end
+>>>>>>> 4d94b4c1fb53b07c051b2cceaae7d795db9ea5a4
